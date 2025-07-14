0796d645df089cc6f17f4371bdcd8b7e10addf97
Roman <zeniaroman@gmail.com>
2025-07-14 21:09:37 +0100

    update for new domain images

--- js/8213.5dae70dd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[8213],{71241:function(c,p){p.A=(c,p)=>{const s=c.__vccOpts||c;for(const[n,t]of p)s[n]=t;return s}}}]);
-//# sourceMappingURL=8213.5dae70dd.js.map
