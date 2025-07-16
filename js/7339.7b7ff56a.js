021813b15c96167f3efeff145d2caf967a038074
Roman <zeniaroman@gmail.com>
2025-07-16 16:06:11 +0100

    update for ai frame

--- js/7339.7b7ff56a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[7339],{71241:function(c,p){p.A=(c,p)=>{const s=c.__vccOpts||c;for(const[n,t]of p)s[n]=t;return s}}}]);
-//# sourceMappingURL=7339.7b7ff56a.js.map
